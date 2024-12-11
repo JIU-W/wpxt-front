a34a5121d4e132ddb85976a9189f4c50456083f2
JIU-W <2661727280@qq.com>
2024-12-11 11:56:24 +0800

    完整代码

---
+++ vite.config.js
@@ -0,0 +1,58 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  define: {
+    'process.env': {}
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+
+  devServer: {
+    //disableHostCheck: true,
+    allowedHosts: 'all',
+  },
+
+  server: {
+    port: 1024,
+    hmr: true,
+    //secure: false,
+    proxy: {
+      "/api": {
+        target: "http://localhost:7090",
+        //target: "http://sdnsrtns.natapp1.cc",
+        changeOrigin: true,
+        pathRewrite: {
+          "^api": "/api"
+        }
+      }
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 3000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    },
+    chunkFileNames: (chunkInfo) => {
+      const facadeModuleId = chunkInfo.facadeModuleId
+        ? chunkInfo.facadeModuleId.split('/')
+        : [];
+      const fileName =
+        facadeModuleId[facadeModuleId.length - 2] || '[name]';
+      return `js/${fileName}/[name].[hash].js`;
+    }
+  }
+})
